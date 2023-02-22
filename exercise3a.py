bc3df466ef5ec728567471039ec2dcc09052a4f1
jvirgil20 <jlv89@miami.edu>
2023-02-22 15:36:42 -0500

    exercise 3a python/ruby added

---
+++ exercise3a.py
@@ -0,0 +1,7 @@
+# example of immutable string in python
+
+s = "string"
+print(s)
+s[1] = "p"
+print(s)
+
---
+++ exercise3a.rb
@@ -0,0 +1,6 @@
+# example of mutable string in ruby
+
+s = "string"
+puts s
+s[1] = "p"
+puts s
